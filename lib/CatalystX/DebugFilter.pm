acc770b811102d4777c27b80a77c8b0de2d506a4
Brian Phillips <bphillips@cpan.org>
2010-08-30 14:19:38 +0000

    Import of BPHILLIPS/CatalystX-DebugFilter-0.04 from CPAN.
    gitpan-cpan-distribution: CatalystX-DebugFilter
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BPHILLIPS/CatalystX-DebugFilter-0.04.tar.gz
    gitpan-cpan-author:       BPHILLIPS
    gitpan-cpan-maturity:     released

--- lib/CatalystX/DebugFilter.pm
+++ lib/CatalystX/DebugFilter.pm
@@ -1,10 +1,12 @@
 package CatalystX::DebugFilter;
-$CatalystX::DebugFilter::VERSION = '0.03';
+BEGIN {
+  $CatalystX::DebugFilter::VERSION = '0.04';
+}
 
 # ABSTRACT: Provides configurable filtering of data that is logged to the debug logs (and error screen)
 use Moose::Role;
 use namespace::autoclean;
-use Clone qw(clone);
+use Clone::Fast qw(clone);
 requires('dump_these','log_request_headers','log_response_headers');
 our $CONFIG_KEY = __PACKAGE__;
 my %filters = (
@@ -149,7 +151,7 @@ CatalystX::DebugFilter - Provides configurable filtering of data that is logged 
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 =head1 SYNOPSIS
 
